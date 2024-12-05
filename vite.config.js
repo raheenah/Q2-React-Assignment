64318fd45521c48d93d41d2bb7274134a61f80b4
Mohammed Raheenat Sunmisola <raheenah500@gmail.com>
2024-12-06 00:48:51 +0100

    Assignment objectives completed, minimal styling done

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-// postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// // postcss.config.js
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
