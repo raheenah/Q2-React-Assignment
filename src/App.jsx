0c27c5853f5f86e5a53404ad8eaddeafbf41412b
Mohammed Raheenat Sunmisola <raheenah500@gmail.com>
2024-12-11 10:48:29 +0100

    rote fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
-
 import StarRatingPage from "./starRating/StarRatingPage";
 import GridPage from "./grid/gridPage";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <nav className='bg-red-400 font-extrabold md:w-[60%] mx-auto rounded-full my-4'>
         <ul className='flex justify-center gap-4'>
-          <NavLink to='/'>Grid Assignment</a>
-          <NavLink to='/star'>Star Assignment</a>
+          <NavLink to='/'>Grid Assignment</NavLink>
+          <NavLink to='/star'>Star Assignment</NavLink>
         </ul>
       </nav>
       <Routes>
